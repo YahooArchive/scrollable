723fde372a047e10df8b618aa1f519e62ed6cea5
Irae <irae@irae.pro.br>
2015-02-27 08:46:55 -0800

    Add transitions to reminders example

--- examples/reminders/index.js
+++ examples/reminders/index.js
@@ -24,35 +24,78 @@ var ReminderCartegory = require('./category');
       {title: "Orange", color: "orange", uid: "1235"},
     ];
     var category_ordering = ["2485","0891","1248","3436","2384","2394","5330","9023","1284","0934","1723","1483","1245","1235"];
-    var stackedSize = 76;
-    var spaceAtBottom = 60;
-    var friction = 600;
 
+
+var stackedSize = 76;
+var spaceAtBottom = 60;
+var friction = 600;
+var transitionTime = 300; // ms
 
 var Reminders = React.createClass({
 
-  getContentSize: function (items, scroller) {
+  getInitialState: function() {
     return {
-      width: scroller.rect.width,
-      height: stackedSize * categories.length,
+      mode: 'all',
+      selected: null,
     };
+  },
+
+  getContentSize: function (items, scroller) {
+    if (this.state.mode === 'all') {
+      return {
+        width: scroller.rect.width,
+        height: stackedSize * categories.length,
+      };
+    } else {
+      return {
+        width: scroller.rect.width,
+        height: items["category_"+this.state.selected].rect.height,
+      };
+    }
   },
 
   showCategory: function(category, event) {
     event.preventDefault();
-    this.refs.scroller.disable();
+    this.refs.scroller.animateAndResetScroll(0, 0);
+    this.setState({
+      mode: 'single',
+      selected: category.uid,
+    });
+  },
+
+  showList: function(event) {
+    event.preventDefault();
+    this.setState({
+      mode: 'all',
+      selected: null,
+    }, function() {
+      this.refs.scroller.animateAndResetScroll(0, 0);
+    });
+
   },
 
   render: function () {
     var self = this;
     var scrollingCategories = categories.map(function(category) {
+      var handler = listScroll;
+      if (self.state.mode === 'single') {
+        handler = self.state.selected === category.uid ? selectedItemHandler : stackedItemHandler;
+      }
       return (
         <ScrollItem name={"category_"+category.uid}
                     key={category.uid}
                     model={category}
-                    scrollHandler={listScroll}>
+                    selectedUid={self.state.selected}
+                    transitionStyles={transitionStyles}
+                    scrollHandler={handler}>
           <Hammer component={ReminderCartegory} {...category}
-                      onTap={self.showCategory.bind(self, category)} />
+                      onTap={function(event) {
+                        if (self.state.mode === 'all') {
+                          self.showCategory(category, event);
+                        } else if(self.state.selected !== category.uid) {
+                          self.showList(event);
+                        }
+                      }} />
         </ScrollItem>
       );
     });
@@ -90,6 +133,42 @@ function listScroll(x, y, self, items, scroller) {
     height: (scroller.rect.height - spaceAtBottom) + 'px',
     zIndex: 2 + order,
     y: pos,
+  };
+}
+
+function stackedItemHandler(x, y, self, items, scroller) {
+  var order = category_ordering.indexOf(self.props.model.uid);
+  var zIndex = 2 + order;
+  var selectedIndex = category_ordering.indexOf(self.props.selectedUid);
+  if (order >= selectedIndex) {
+    order = order - 1;
+  }
+
+  var pos = scroller.rect.height + 5; // off screen
+  var numStacked = Math.min(5, category_ordering.length-1);
+  if (order <= numStacked) {
+    pos = scroller.rect.height - spaceAtBottom + 10 + ( (order+1) * (spaceAtBottom-30) / numStacked );
+  }
+
+  return {
+    height: (scroller.rect.height - spaceAtBottom) + 'px',
+    y: pos,
+    zIndex: zIndex,
+  };
+}
+
+function selectedItemHandler(x, y, self, items, scroller) {
+  var order = category_ordering.indexOf(self.props.model.uid);
+  return {
+    zIndex: 2 + order,
+    y: -y,
+  };
+}
+
+function transitionStyles(self, items, scroller) {
+  return {
+    duration: transitionTime,
+    property: '-webkit-transform',
   };
 }
 
