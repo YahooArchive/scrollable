4c6479fb2c0f61945e63d56809119a4c13d67afb
Irae <irae@irae.pro.br>
2015-03-04 00:37:05 -0800

    More realistic coverage if we include all not tested files

--- karma.shared.js
+++ karma.shared.js
@@ -13,7 +13,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      {pattern: 'src/**/__tests__/*-test.js', watched: true, included: true, served: true},
+      {pattern: 'src/**/*.js', watched: true, included: true, served: true},
       {pattern: 'src/**/*.gif', watched: false, included: false, served: true},
     ],
 
