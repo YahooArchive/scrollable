8d7b88580251827dbbda027ce1003f421c2059a5
Irae <irae@irae.pro.br>
2015-03-05 02:08:27 -0800

    Add real consumption mode on consumption mode example

--- Readme.md
+++ Readme.md
@@ -15,21 +15,13 @@ Scrollable components for advanced mobile scrolling with React.js
 
   * WebkitTransform -> MozTransform -> transform detection
 
-  * While changing the code to use DOM directly instead of React, I actually
-    wrote some code to cleanup previous styles. I am not sure the lib should
-    do it, so I left it commented for now. There are very low perceived
-    issues on iPhone 4 with the cleanup, (guessing from 60 to 40 fps?)
-
-  * I was lazy and didn't complete the consumption mode example, will need to
-    finish later. For not it has only top and bottom sidebar handling, but no
-    consumption transition itself.
-
   * Styles are currently only on the examples, need to figure out a way to
     be in JavaScript and still be able to override via some API.
 
-  * Figure out the best way to require the components and at the same time provide
-    a package that whoever is not using CommonJS can still have a good API.
+  * Figure out the best way to require the components and at the same time
+    provide a package that whoever is not using CommonJS can still have a
+    nice API for bower or manual download.
 
-  * Forcing `<Scroller>` to declare scrollingX and scrollingY for now. There is
-    potential to inferring this based on itemSizes, but Zynga's API don't provide
-    a way of updating this value in runtime.
+  * Forcing `<Scroller>` to declare scrollingX and scrollingY for now. There
+    is potential to inferring this based on itemSizes, but Zynga's API don't
+    provide a way of updating this value in runtime.
--- examples/consumption/index.js
+++ examples/consumption/index.js
@@ -6,12 +6,44 @@ var ScrollItem = require('../../src/scroll-item');
 var TopBar = require('./top');
 var BotBar = require('./bot');
 var Lorem = require('../lorem');
+var Hammer = require('hammerjs');
 
 var ConsumptionMode = React.createClass({
 
+  render: function () {
+    return (
+      <div className="consumption">
+        <Scroller viewport scrollingX={false} scrollingY={true}
+                  getContentSize={this.getContentSize} ref="scroller">
+
+          <ScrollItem ref="topbar" name="topbar" scrollHandler={consumptionBars}>
+            <TopBar />
+          </ScrollItem>
+
+          <ScrollItem name="content" scrollHandler={contentHandler}>
+            <div ref="swipeArea">
+              <Lorem />
+            </div>
+          </ScrollItem>
+
+          <ScrollItem name="botbar" scrollHandler={consumptionBars}>
+            <BotBar />
+          </ScrollItem>
+
+        </Scroller>
+      </div>
+    );
+  },
+
   getContentSize: function (items, scroller) {
-    var height = 0;
-    var width = 0;
+    /*
+      This is a very generic contentSize implementation, accounting for all
+      items regardless of semantics of each item. It's not a coincidence that a
+      regular for loop is used. map/reduce/forEach creates functions in runtime.
+      If you scrape each piece of performance, you can probably run this example
+      on iPhone 3GS or older Androids.
+    */
+    var height = 0, width = 0;
     for (var item in items) {
       height += items[item].rect.height;
       width = Math.max(width, items[item].rect.width);
@@ -22,43 +54,77 @@ var ConsumptionMode = React.createClass({
     };
   },
 
-  render: function () {
-    return (
-      <div className="consumption">
-        <Scroller viewport scrollingX={false} scrollingY={true} getContentSize={this.getContentSize}>
-          <ScrollItem name="topbar" scrollHandler={topHandler}>
-            <TopBar />
-          </ScrollItem>
-          <ScrollItem name="content" scrollHandler={contentHandler}>
-            <Lorem />
-          </ScrollItem>
-          <ScrollItem name="botbar" scrollHandler={botHandler}>
-            <BotBar />
-          </ScrollItem>
-        </Scroller>
-      </div>
-    );
-  }
+  componentDidMount: function() {
+    this.refs.scroller.consuming = true;
+    this.refs.scroller.origin = 0;
+    var hammer = (this.hammer = new Hammer(this.refs.swipeArea.getDOMNode()));
+    hammer.get('swipe').set({
+      direction: Hammer.DIRECTION_DOWN,
+      velocity: 0.3,
+      threshold: 7,
+    });
+    hammer.on('swipe', this.swipeDown);
+  },
+
+  componentWillUnmount: function() {
+    this.hammer.off('swipe');
+  },
+
+  swipeDown: function() {
+    var scroller = this.refs.scroller;
+    if (!scroller.consuming) {
+      scroller.consuming = true;
+      scroller.origin = scroller._scroller._scroller.getValues().top - this.refs.topbar.rect.height;
+    }
+  },
 
 });
 
 
 /*
-Notice: The following functions are outside of the React component.
-Read more about why on the minimal example.
+  Notice: The following functions are outside of the React component.
+  Read more about why on the minimal example.
 */
-function topHandler(x, y, self, items, scroller) {
+function consumptionBars(x, y, self, items, scroller) {
+  // All calculations are made for top handler, then inverted in the end
+  // if this call refers to the bottom handler
+
+  // If we are in the middle of the content and scrolled past the swipe origin
+  // keeps reseting origin until user scrolls down. Effectively this means the
+  // first time user scrolls down starts hiding the bars again.
+  if (y > 0 && y < scroller.origin && scroller.consuming) {
+    scroller.origin = y;
+  }
+
+  // if we are near the top, force topBar to show
+  if (y <= self.rect.height && !scroller.consuming) {
+    scroller.consuming = true;
+    scroller.origin = 0;
+  }
+
+  var pos;
+  if (scroller.consuming) {
+    pos = Math.min(scroller.origin - y, 0);
+  } else {
+    pos = -self.rect.height; // offscreen
+  }
+
+  // As the top bar moves offscreen, it should be locked offscreen
+  if (pos <= -self.rect.height) {
+    scroller.consuming = false;
+  }
+
+  // If this was called from botbar, make it work from bottom of the viewport.
+  if (self === items.botbar) {
+    pos = scroller.rect.height - self.rect.height - pos;
+  }
+
   return {
-    y: 0,
+    y: pos,
     zIndex: 5,
   };
 }
-function botHandler(x, y, self, items, scroller) {
-  return {
-    y: scroller.rect.height - self.rect.height,
-    zIndex: 5,
-  };
-}
+
 function contentHandler(x, y, self, items, scroller) {
   return {
     y: items.topbar.rect.bottom - y,
